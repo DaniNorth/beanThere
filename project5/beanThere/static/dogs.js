45ce90c158b2a87149ad0017eccc573b3902c658
DaniNorth <112745991+DaniNorth@users.noreply.github.com>
2024-04-05 09:42:09 -0700

    Update dogs.js
    delete old project code

--- project5/beanThere/static/dogs.js
+++ project5/beanThere/static/dogs.js
@@ -1,13 +1,1 @@
-document.addEventListener("DOMContentLoaded", ()=> {
-    document.querySelector("#dog-form").addEventListener(submitDogForm)
-    })
 
-    function submitDogForm(event) {
-        event.preventDefault()
-        let dog_form = new FormData(document.querySelector("#dog-form"))
-
-        fetch("/dogs/", {
-            method: 'post',
-            body: dog_form
-        }).then(response => response.json()).then(data =>)
-    }
