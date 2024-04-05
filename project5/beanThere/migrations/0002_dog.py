6431eacad70844c94c833df339ef517349232364
DaniNorth <112745991+DaniNorth@users.noreply.github.com>
2024-04-05 09:40:08 -0700

    Update 0002_dog.py
    delete old project code

--- project5/beanThere/migrations/0002_dog.py
+++ project5/beanThere/migrations/0002_dog.py
@@ -3,18 +3,3 @@
 from django.db import migrations, models
 
 
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ('network', '0001_initial'),
-    ]
-
-    operations = [
-        migrations.CreateModel(
-            name='Dog',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=30)),
-            ],
-        ),
-    ]
